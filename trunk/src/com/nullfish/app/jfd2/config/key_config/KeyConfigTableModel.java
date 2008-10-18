c32533f8915a825d1b65da75fa1a72ca51f39ccd
ikemen <ikemen@63c50bf9-1d40-0410-92c2-f3bfeb6eac0f>
2008-10-18 15:41:48 +0000

    コミット漏れが合ったんで追加

---
+++ trunk/src/com/nullfish/app/jfd2/config/key_config/KeyConfigTableModel.java
@@ -0,0 +1,48 @@
+package com.nullfish.app.jfd2.config.key_config;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.swing.table.AbstractTableModel;
+
+import org.jdom.Document;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
+
+import com.nullfish.app.jfd2.command.CommandManager;
+import com.nullfish.lib.vfs.VFile;
+import com.nullfish.lib.vfs.exception.VFSException;
+
+public class KeyConfigTableModel extends AbstractTableModel {
+
+	private List commands = new ArrayList();
+	
+	private Map commandDefaultKeysMap = new HashMap();
+	
+	private Map commandCustomKeysMap = new HashMap();
+	
+	public KeyConfigTableModel(VFile baseDir) throws JDOMException, IOException, VFSException {
+		Document defaultCommandDoc = new Document();
+		SAXBuilder builder = new SAXBuilder();
+		builder.build(baseDir.getChild(CommandManager.COMMAND_FILE).getInputStream());
+	}
+	
+	public int getColumnCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getRowCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
